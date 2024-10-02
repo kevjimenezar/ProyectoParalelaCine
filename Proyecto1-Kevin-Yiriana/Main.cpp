4a520157b19a0b09ef26ce66f2d887b38ea74e02
Nivek-12 <kevin.jimenez.ara@gmail.com>
2024-10-02 12:18:48 -0600

    Create Data

--- Proyecto1-Kevin-Yiriana/Main.cpp
+++ Proyecto1-Kevin-Yiriana/Main.cpp
@@ -2,6 +2,156 @@
 
 using namespace std;
 
+
+void llenarAsientosAleatorios(ContAsientos* contAsientos) {
+	srand((unsigned)time(0));
+
+	for (int i = 0; i < 6; i++) {
+		for (int j = 0; j < 10; j++) {
+			Asiento* asiento = new Asiento();
+
+			int estadoAleatorio = rand() % 2; // 0 para 'V', 1 para 'R'
+			if (estadoAleatorio == 0) {
+				asiento->setEstado('V'); // Vacío
+			}
+			else {
+				asiento->setEstado('R'); // Reservado
+				asiento->setCedula("Ced-" + to_string(rand() % 10000 + 10000)); // Cedula aleatoria
+			}
+
+			asiento->setIdentificador(asiento->identAsiento(i, j));
+			contAsientos->ingresaAsiento(asiento);
+		}
+	}
+}
+
+void crearDatosQuemados(ContCinema* contCinema) {
+
+	// Crear las películas
+	Pelicula* peliculas[] = {
+		new Pelicula("Interstellar", "Ciencia Ficcion", "Subtitulada", "2D", "Adultos", "Exploracion espacial", "USA", 2014),
+		new Pelicula("The Dark Knight", "Accion", "Doblada", "2D", "Adultos", "Superheroes y crimen", "USA", 2008),
+		new Pelicula("Inception", "Thriller", "Subtitulada", "3D", "Adultos", "Suenos y realidad", "USA", 2010),
+		new Pelicula("Toy Story", "Animacion", "Doblada", "2D", "Ninios", "Aventura infantil", "USA", 1995),
+		new Pelicula("Avengers: Endgame", "Accion", "Subtitulada", "3D", "Adultos", "Superheroes", "USA", 2019),
+		new Pelicula("La La Land", "Musical", "Doblada", "2D", "Adultos", "Suenos y amor", "USA", 2016),
+		new Pelicula("Parasite", "Thriller", "Subtitulada", "2D", "Adultos", "Clase y sociedad", "Corea del Sur", 2019),
+		new Pelicula("Frozen II", "Animacion", "Doblada", "2D", "Ninios", "Aventura magica", "USA", 2019),
+		new Pelicula("Joker", "Drama", "Subtitulada", "2D", "Adultos", "Psicologia y crimen", "USA", 2019),
+		new Pelicula("The Matrix", "Ciencia Ficcion", "Subtitulada", "2D", "Adultos", "Realidad virtual", "USA", 1999),
+		new Pelicula("Coco", "Animacion", "Doblada", "2D", "Ninios", "Familia y tradiciones", "Mexico", 2017),
+		new Pelicula("Spider-Man: No Way Home", "Accion", "Subtitulada", "3D", "Adultos", "Superheroes y multiverso", "USA", 2021)
+	};
+
+
+	// Crear funciones para las películas
+	Funcion* funciones[] = {
+		new Funcion(10, 9, "10/10/2024", peliculas[0]),   // Mañana
+		new Funcion(15, 14, "10/10/2024", peliculas[1]),  // Tarde
+		new Funcion(20, 19, "10/10/2024", peliculas[2]),  // Noche
+		new Funcion(5, 10, "10/10/2024", peliculas[3]),   // Mañana
+		new Funcion(25, 15, "10/10/2024", peliculas[4]),  // Tarde
+		new Funcion(30, 20, "10/10/2024", peliculas[5]),  // Noche
+		new Funcion(45, 11, "11/10/2024", peliculas[6]),  // Mañana
+		new Funcion(50, 16, "11/10/2024", peliculas[7]),  // Tarde
+		new Funcion(35, 21, "11/10/2024", peliculas[8]),  // Noche
+		new Funcion(55, 9, "11/10/2024", peliculas[9]),   // Mañana
+		new Funcion(40, 14, "11/10/2024", peliculas[10]), // Tarde
+		new Funcion(25, 19, "11/10/2024", peliculas[11])  // Noche
+	};
+
+	// Crear las salas
+	Sala* sala1 = new Sala();
+	Sala* sala2 = new Sala();
+	Sala* sala3 = new Sala();
+	Sala* sala4 = new Sala();
+
+	// Crear contenedores de asientos aleatorios
+	ContAsientos* contAsientosSala1_1 = new ContAsientos();
+	ContAsientos* contAsientosSala1_2 = new ContAsientos();
+	ContAsientos* contAsientosSala1_3 = new ContAsientos();
+
+	ContAsientos* contAsientosSala2_1 = new ContAsientos();
+	ContAsientos* contAsientosSala2_2 = new ContAsientos();
+	ContAsientos* contAsientosSala2_3 = new ContAsientos();
+
+	ContAsientos* contAsientosSala3_1 = new ContAsientos();
+	ContAsientos* contAsientosSala3_2 = new ContAsientos();
+	ContAsientos* contAsientosSala3_3 = new ContAsientos();
+
+	ContAsientos* contAsientosSala4_1 = new ContAsientos();
+	ContAsientos* contAsientosSala4_2 = new ContAsientos();
+	ContAsientos* contAsientosSala4_3 = new ContAsientos();
+
+	llenarAsientosAleatorios(contAsientosSala1_1);
+	llenarAsientosAleatorios(contAsientosSala1_2);
+	llenarAsientosAleatorios(contAsientosSala1_3);
+
+	llenarAsientosAleatorios(contAsientosSala2_1);
+	llenarAsientosAleatorios(contAsientosSala2_2);
+	llenarAsientosAleatorios(contAsientosSala2_3);
+
+	llenarAsientosAleatorios(contAsientosSala3_1);
+	llenarAsientosAleatorios(contAsientosSala3_2);
+	llenarAsientosAleatorios(contAsientosSala3_3);
+
+	llenarAsientosAleatorios(contAsientosSala4_1);
+	llenarAsientosAleatorios(contAsientosSala4_2);
+	llenarAsientosAleatorios(contAsientosSala4_3);
+
+	// Asignar las funciones y los contenedores de asientos a las salas
+	sala1->ingresarFuncion(funciones[0]);
+	sala1->ingresarFuncion(funciones[1]);
+	sala1->ingresarFuncion(funciones[2]);
+	sala1->ingresaMatrizAsientos(contAsientosSala1_1);
+	sala1->ingresaMatrizAsientos(contAsientosSala1_2);
+	sala1->ingresaMatrizAsientos(contAsientosSala1_3);
+	sala1->setTipoSala("VIP");
+
+	sala2->ingresarFuncion(funciones[3]);
+	sala2->ingresarFuncion(funciones[4]);
+	sala2->ingresarFuncion(funciones[5]);
+	sala2->ingresaMatrizAsientos(contAsientosSala2_1);
+	sala2->ingresaMatrizAsientos(contAsientosSala2_2);
+	sala2->ingresaMatrizAsientos(contAsientosSala2_3);
+	sala2->setTipoSala("Regular");
+
+	sala3->ingresarFuncion(funciones[6]);
+	sala3->ingresarFuncion(funciones[7]);
+	sala3->ingresarFuncion(funciones[8]);
+	sala3->ingresaMatrizAsientos(contAsientosSala3_1);
+	sala3->ingresaMatrizAsientos(contAsientosSala3_2);
+	sala3->ingresaMatrizAsientos(contAsientosSala3_3);
+	sala3->setTipoSala("VIP");
+
+	sala4->ingresarFuncion(funciones[9]);
+	sala4->ingresarFuncion(funciones[10]);
+	sala4->ingresarFuncion(funciones[11]);
+	sala4->ingresaMatrizAsientos(contAsientosSala4_1);
+	sala4->ingresaMatrizAsientos(contAsientosSala4_2);
+	sala4->ingresaMatrizAsientos(contAsientosSala4_3);
+	sala4->setTipoSala("Regular");
+
+	// Crear los cines y agregarlos al contenedor de cines
+	Cinema* cine1 = new Cinema(sala1, sala2);
+	Cinema* cine2 = new Cinema(sala3, sala4);
+
+	cine1->setNiReg(10);
+	cine1->setNiVIP(50);
+	cine1->setAdulReg(20);
+	cine1->setAdulVIP(70);
+
+	cine2->setNiReg(15);
+	cine2->setNiVIP(30);
+	cine2->setAdulReg(25);
+	cine2->setAdulVIP(50);
+
+	contCinema->ingresaCinema(cine1);
+	contCinema->ingresaCinema(cine2);
+}
+
+
+
 int main() {
 
 	string nom;					//Pelicula
@@ -38,6 +188,7 @@ int main() {
 	Asiento* ptrAsient = NULL;
 	ContAsientos* CA = NULL;
 	ContCinema* CO = new ContCinema();
+	crearDatosQuemados(CO);
 
 	while(op != 3){
 		system("CLS");
@@ -147,10 +298,10 @@ int main() {
 					cout << "---------------------------------Menu de busqueda de pelicula-----------------------------------------" << endl
 						<< "1) Desea hacer la busqueda por genero?" << endl
 						<< "2) Desea hacer la busqueda por nombre?" << endl
-						<< "3) Desea hacer la busqueda por tipo? (doblada o subtitulada)" << endl
+						<< "3) Desea hacer la busqueda por tipo? (Doblada o Subtitulada)" << endl
 						<< "4) Desea hacer la busqueda por dimenciones? (2D O 3D)" << endl
 						<< "5) Desea hacer la busqueda por sala? (VIP o Regular)" << endl
-						<< "6) Desea hacer la busqueda por tipo de publico? (ninios o adultos)" << endl
+						<< "6) Desea hacer la busqueda por tipo de publico? (Ninios o Adultos)" << endl
 						<< "7) Salir" << endl
 						<< "-------------------------------------------------------------------------------" << endl
 						<< "   Digite su opcion: ";
